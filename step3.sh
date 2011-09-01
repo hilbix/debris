46daba2536cb62f6c6f827c59ca019ac0114689f
Valentin Hilbig <webmaster@scylla-charybdis.com>
2011-09-01 20:21:03 +0000

    still not ready step3

--- step3.sh
+++ step3.sh
@@ -2,14 +2,64 @@
 
 . ./lib.sh
 
-copy run.sh /ins/run.sh
+mksources()
+{
+echo " deb     $MIRROR $RELEASE           main contrib non-free"
+echo " deb     http://security.debian.org/  $RELEASE/updates   main contrib non-free"
 
+case "$RELEASE" in
+squeeze)
+	echo " deb     $MIRROR $RELEASE-updates   main contrib non-free"
+	;;
+esac
+}
+
+debdoubler()
+{
+sed -n 'p;s/deb     /deb-src /p'
+}
+
+mkinterfaces()
+{
+cat <<EOF
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet dhcp
+EOF
+}
+
+mkfstab()
+{
+cat <<EOF
+proc /proc    proc     defaults             0 0
+none /dev     devtmpfs mode=0755            0 0
+none /sys     sysfs    noexec,nosuid,nodev  0 0
+EOF
+}
+
+echo "$HOSTNAME" | copy /ins/etc/hostname
+echo "$DOMAINNAME"
+
+mksources | debdoubler | copy /ins/etc/apt/sources.list
+mkinterfaces | copy /ins/etc/network/interfaces
+mkfstab | copy /ins/etc/fstab
+
+copy run.sh /ins/run.sh
 callsub chroot /ins /run.sh
 
 procedure <<EOF
 
-mount -t proc proc /proc
-mount -t devtmpfs -o mode=0755 none /dev
-mount -t sysfs -o noexec,nosuid,nodev none /sys
+ok umount /dev
+ok umount /sys
+ok umount /proc
 
+mount /proc
+mount /dev
+mount /sys
+
+apt-get update
+apt-get install $KERN $PACKAGES
 EOF
+
