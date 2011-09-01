dcc30158cba18a87c6ab2ab77f840b552b71c48a
Valentin Hilbig <valentin@hilbig.de>
2011-09-01 16:21:41 +0200

    OOPS, last comment was wrong, it should have been spelled: Adding more to lib.sh
    This one here limits it to root.
    
    (Well, I am too used to how CVS worked.  Sorry)

---
+++ libconnect.sh
@@ -0,0 +1,31 @@
+#
+# Subroutines to create the connection
+#
+# FD3 input FD (shell reads data from remote)
+# FD4 output FD (shell writes data to remote)
+#
+# Copyright (C) 2011 Valentin Hilbig <webmaster@scylla-charybdis.com>
+# License see lib.sh
+
+conn_ssh()
+{
+ptybuffer -cfo DEBUG.log .sock ssh -acxqe none "$@" ./run.sh
+}
+
+conn_local()
+{
+ptybuffer -cfo DEBUG.log .sock ./run.sh
+}
+
+# Fork off new script, killing the old one, just in case
+while	conn_$CONNECTIONTYPE $CONNECTIONARGS
+	[ 42 = $? ]
+do
+	timeout 2 ptybufferconnect -qnp bye .sock
+	timeout 2 ptybufferconnect -qnp "$EOF" .sock
+	timeout 2 ptybufferconnect -qnp "$INTR" .sock
+done
+
+exec 3< <(ptybufferconnect -n .sock) || OOPS "cannot connect to input socket"
+exec 4> >(socat - unix:.sock) || OOPS "cannot connect to output socket"
+
