2b932065bd50a5805def6fb875ba90eb5ee93b02
Valentin Hilbig <webmaster@scylla-charybdis.com>
2018-09-08 21:22:37 +0200

    cmd.h modified

--- cmd.h
+++ cmd.h
@@ -13,6 +13,9 @@
 #ifndef	DEBRIS_VADD
 #define	DEBRIS_VADD(...)
 #endif
+#ifndef	DEBRIS_TOPIC
+#define	DEBRIS_TOPIC(...)
+#endif
 
 #define	V(AUTO,NAME,DEFAULT,HELP)	\
                                         DEBRIS_VADD(#NAME, AUTO, DEFAULT, HELP)
@@ -21,7 +24,9 @@
                                         DEBRIS_CNAME(NAME)		\
                                         DEBRIS_CFN(NAME,FN)		\
                                         DEBRIS_CALL(NAME,MIN,MAX)	\
-                                        DEBRIS_CADD(cmd_##NAME, #NAME, MIN, MAX, USAGE, HELP)
+                                        DEBRIS_CADD(#NAME, CMD(NAME), MIN, MAX, USAGE, HELP)
+
+#define H(NAME,HELP)			DEBRIS_TOPIC(#NAME, HELP)
 
 V(1, prompt,	"[{$prefix}]{$loc}@{$name}:{#dir}$ ",	"DebRIS prompt")
 V(0, prefix,	"DebRIS",				"DebRIS prompt prefix")
@@ -29,6 +34,7 @@ V(0, name,	"{=hostname}",				"DebRIS name")
 V(0, loc,	"{=getpid}",				"DebRIS location")
 V(1, dir,	"{=cwd}",				"DebRIS directory")
 V(0, sep,	"\t",					"separator for echo")
+V(1, err,	"",					"automatic error variable (error string of last command)")
 
 C(exit, 0,1, "[n]", "exit DebRIS with given return code, default: 0",
 {
@@ -39,40 +45,62 @@ C(exit, 0,1, "[n]", "exit DebRIS with given return code, default: 0",
   return 0;
 })
 
-C(help, 0, 1, "[command]", "list available commands or explain command",
+H(usage, "{arg0} 'command'..\n"
+  "")
+C(help, 0, 2, "[command] | help [topic]", "explain command or topic",
 {
   const struct _debris_cmd	*cmd;
+  const struct _debris_topic	*topic;
   const char			*p;
+  int				pos;
 
   if (!args[0])
     {
       tino_hash_iter		iter;
 
-      for (tino_hash_iter_start(&iter, &D->cmds, 0); (cmd=tino_hash_iter_data_rawptr(&iter))!=0; tino_hash_iter_next(&iter))
-        printf("%s %s%*s%.*s\n", cmd->name, cmd->usage, (int)(D->cmdmaxwidth-strlen(cmd->name)-strlen(cmd->usage)), "", tino_str_nclen(cmd->help, '\n'), cmd->help);
+      for (tino_hash_iter_start(&iter, &D->map_cmd, 0); (cmd=tino_hash_iter_data_rawptr(&iter))!=0; tino_hash_iter_next(&iter))
+        outp(D, "%s %s%*s%.*s\n", cmd->name, cmd->usage, (int)(D->cmdmaxwidth-strlen(cmd->name)-strlen(cmd->usage)), "", tino_str_nclen(cmd->help, '\n'), cmd->help);
       return 0;
     }
   cmd	= debris_cmd(D, args[0], 0);
   if (!cmd)
     return "unknown command";
 
-   printf("%s %s\n", cmd->name, cmd->usage);
-   for (p=cmd->help; *p; p++)
-     {
-       int	c;
+  if (cmd!=D->currentcmd && args[1])
+    return "command do not have topics, try 'help help topic'";
+  outp(D, "%s %s\n", cmd->name, cmd->usage);
+  for (p=cmd->help; *p; p++)
+    {
+      pos	= tino_str_nclen(p, '\n');
+      outp(D, "\t%.*s\n", pos, p);
+      p	+= pos;
+      if (!*p)
+        break;
+    }
 
-       c	= tino_str_nclen(p, '\n');
-       printf("\t%.*s\n", c, p);
-       p	+= c;
-       if (!*p)
-         break;
-     }
-   return 0;
+  if (!args[1])
+    return 0;
+
+  topic	= debris_topic(D, args[1], 0);
+  if (!topic)
+    return "unknown topic";
+  pos	= tino_str_nclen(topic->help, '\n');
+  outp(D, "%s: %.*s\n", topic->name, pos, topic->help);
+  while (topic->help[pos++]=='\n')
+    {
+      int	len;
+
+      len	= tino_str_nclen(topic->help+pos, '\n');
+      outp(D, "\t%.*s\n", len, topic->help+pos);
+      pos	+= len;
+    }
+
+  return 0;
 })
 
-C(echo, 0, -1, "[args..]", "echo the given args, separated (see sep)",
+C(echo, 0, -1, "[args..]", "echo the given args uninterpreted and separated (see var sep)",
 {
-  cmd_echoc(D, args);
+  CMD(echoc)(D, args);
   outln(D, "");
   return 0;
 })
@@ -84,19 +112,25 @@ C(echoc, 0, -1, "[args..]", "like echo, without NL at the end",
   return 0;
 })
 
-C(print, 0, -1, "[args..]", "print arguments, without sparator, without NL",
+C(print, 0, -1, "[args..]", "evaluate arguments and print them, without sparator, without NL",
 {
+  DP(("(%p)", args));
   while (*args)
     {
+      struct debris_expr	*e;
+
+      DP(("() %s", *args));
       outsep(D, NULL);
-      outesc(D, *args++);
+      e	= EXPR(eval)(D, *args++);
+      outesc(D, EXPR(str)(e));
+      expr_free(e);
     }
   return 0;
 })
 
 C(println, 0, -1, "[args..]", "like print, but outputs NL afterwards",
 {
-  cmd_print(D, args);
+  CMD(print)(D, args);
   outln(D, "");
   return 0;
 })
@@ -107,10 +141,10 @@ ino_file_getcwdO());
 tino_str_printf("%ld", (long)getpid()));
 #endif
 
-C(set, 0, -1, "var[=value]..", "set a DebRIS variable to a static value\n\
-If =value is missing, some default value is used",
+C(set, 0, -1, "var[=value]..", "set a DebRIS variable to a static value\n"
+  "If =value is missing, some default value is used",
 {
-  int	unset	= D->now->fn==cmd_unset;
+  int	unset	= D->currentcmd->fn==cmd_unset;
 
   while (*args)
     {
@@ -132,8 +166,8 @@ If =value is missing, some default value is used",
   return 0;
 })
 
-C(unset, 0, -1, "var[=expr]..",  "unset a DebRIS variable or make it automatic\n\
-Predefined variables get their default/automatic value, others are unset",
+C(unset, 0, -1, "var[=expr]..",  "unset a DebRIS variable or make it automatic\n"
+  "Predefined variables get their default/automatic value, others are unset",
 {
   return cmd_set(D, args);
 })
@@ -158,5 +192,6 @@ Predefined variables get their default/automatic value, others are unset",
 #undef	DEBRIS_CFN
 #undef	DEBRIS_CALL
 #undef	DEBRIS_VADD
+#undef	DEBRIS_TOPIC
 #undef	V
 #undef	C
